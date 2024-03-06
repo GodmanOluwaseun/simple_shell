9d4acc2f0240f0bf83f906f1a80454f421d00229
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-03-06 04:21:49 -0800

    UNIX command line interpreter.

--- tokenize.c
+++ tokenize.c
@@ -1,4 +1,47 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * tokenize - tokenizes & Splits copied string using strtok.
+ * @str_cpy: Pointer to copied string to be tokenized.
+ * @delim: Array of delimiters.
+ * @arr_size: Size of array.
+ * @str_token: Pointer to array of pointers to tokens.
+ * Return: Void.
+ */
+
+void tokenize(char *str_cpy, char delim[], int *arr_size, char **str_token)
+{
+	int i;
+	char *token;
+
+	token = strtok(str_cpy, delim);
+	for (i = 0; token != NULL; i++, token = strtok(NULL, delim))
+	{
+		if (i >= *arr_size)
+		{
+		(*arr_size) += 2;
+		str_token = realloc(str_token, sizeof(char *) * (*arr_size));
+			if (str_token == NULL)
+			{
+				perror("Realloc error");
+				free(str_token);
+				free(str_cpy);
+				exit(98);
+			}
+		}
+		str_token[i] = strdup(token);
+		if (str_token[i] == NULL)
+		{
+			perror("Strdup error");
+			free(str_token);
+			free(str_cpy);
+			exit(98);
+		}
+	}
+	str_token[i] = NULL;
+}
 
 /**
  * split_string - Tokenizes and returns array of tokenized a string.
@@ -8,44 +51,26 @@
 
 char **split_string(char *str)
 {
-	char *token, *str_cpy, **str_token, delim[] = {' ', ',', ';', ':'};
-	int arr_size = 15, i = 0;
+	char *str_cpy, **str_token, delim[] = {' ', '.', ',', ';', ':'};
+	int arr_size = 15;
 
 	str_token = malloc(sizeof(char *) * arr_size);
 	if (str_token == NULL)
+	{
+		perror("Malloc error");
 		exit(98);
+	}
 
 	str_cpy = strdup(str);
 	if (str_cpy == NULL)
 	{
+		perror("Strdup error");
 		free(str_token);
 		exit(98);
 	}
-	token = strtok(str_cpy, delim);
-	while (token != NULL)
-	{
-		if (i >= arr_size)
-		{
-			arr_size += 2;
-			str_token = realloc(str_token, sizeof(char *) * arr_size);
-			if (str_token == NULL)
-			{
-				free(str_token);
-				free(str_cpy);
-				exit(98);
-			}
-		}
-		str_token[i] = strdup(token);
-		if (str_token[i] == NULL)
-		{
-			free(str_token);
-			free(str_cpy);
-			exit(98);
-		}
-		token = strtok(NULL, delim);
-		i++;
-	}
-	str_token[i] = NULL;
+
+	tokenize(str_cpy, delim, &arr_size, str_token);
+
 	free(str_cpy);
 	return (str_token);
 }
